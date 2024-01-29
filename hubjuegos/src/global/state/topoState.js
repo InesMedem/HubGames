76f034a6d6644aa4cae77247d5fd1aa636113974
InesMedem <inesmedem93@gmail.com>
2024-01-29 18:35:54 +0100

    compartimentalizando la logica

---
+++ hubjuegos/src/global/state/topoState.js
@@ -0,0 +1,7 @@
+/* Glpobal State - todos los elementos que cambian son 
+let currMoleTile;
+let currPlantTile;
+
+let score = 0;
+let gameOver = false;
+*/
--- hubjuegos/src/pages/Topo/Topo.js
+++ hubjuegos/src/pages/Topo/Topo.js
@@ -1,4 +1,6 @@
 import "./Topo.css";
+import { TopoBoard } from "../../components";
+//import { getRandomTile } from "../../utils";
 
 //! ------------------------------------------------------------------------------
 //? ------------------------------TEMPLATE INICIAL--------------------------------
@@ -7,7 +9,6 @@ const template = () => `
     <div id="bodyMole"> 
       <h1 id="h1"> Wack a Mole </h1>
       <h2 id="score"> 0 </h2>
-
       <div id="board"></div>
     </div>
 `;
@@ -16,51 +17,11 @@ const template = () => `
 //? ------------------------------LOGICA DEL JUEGO -------------------------------
 //! ------------------------------------------------------------------------------
 
-let currMoleTile;
-let currPlantTile;
-let score = 0;
-let gameOver = false;
-
-// function to set up the game
-// set up the grid in HTML
-// create a div element for each tile on the board
-// attach a click event listener to each tile
-// append the tile to the board element in html
-// set the intervals to the board element in the html
-// set up intervals to periodically spawn mole and plan images on the baord
-
-// function to get random tile on the board
-
-// function to set a mole image on a random tile
-
-// clear the previous mole tile
-
-// create a new mole element
-
-// get a random tile, making sure not to overlap with the current plant tile
-
-// set the current mole tile and append the mole image to it
-
-// function to set the plant image on a random tile
-
-// clear the previous plant tile
-
-// create a new plant image element
-
-// get random tile, making sure not to overlap with the current mole tile
-
-// set the current plan tile and append the plant image to it
-
-// function to handel the click event on a tile
-
-// check if the clicked tile is the mole tile
-// increase the score and update the score in the HTML
-// if the clicked tile is the plant tile, end the game and display teh final score
-
 //! ------------------------------------------------------------------------------
 //? ---------------------FUNCION QUE SE EXPORTA QUE PINTA LA PAGINA--------------
 //! ------------------------------------------------------------------------------
+
 export const PrintTopoPage = () => {
   document.querySelector("main").innerHTML = template();
-  setGame();
+  TopoBoard();
 };
